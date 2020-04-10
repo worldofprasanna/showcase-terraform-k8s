cd008be7546768c352d9f956d5fe1037fa722b61
Prasanna V <prasannacse2005@gmail.com>
2020-04-10 20:24:13 +0530

    clean up the unused tf scripts

--- infrastructure_provisioning/ecr.tf
+++
@@ -1,7 +0,0 @@
-resource "aws_ecr_repository" "web" {
-  name = "cats-web"
-}
-
-resource "aws_ecr_repository" "api" {
-  name = "cats-api"
-}
--- infrastructure_provisioning/s3.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_s3_bucket" "chart_repository" {
-  bucket = "toptal_chart_repo_prasanna"
-  acl    = "private"
-
-  tags = {
-    Name        = "Toptal Helm Chart Repo"
-    Reason = "toptal"
-  }
-}
