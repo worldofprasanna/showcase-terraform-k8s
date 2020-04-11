11848a804b44cca89041265b58e3caf8c42c4103
Prasanna V <prasannacse2005@gmail.com>
2020-04-11 13:29:37 +0530

    refactor the ecr install and destroy scripts

--- infrastructure_provisioning/deployer/common.sh
+++ infrastructure_provisioning/deployer/common.sh
@@ -10,3 +10,42 @@ function check_env() {
     exit 1
   fi
 }
+
+function install() {
+  resource=$1
+  env=$2
+
+  check_env $resource $env
+
+  cd $resource
+  terraform plan -out plan.out -var-file="../secrets/$env.tfvars"
+  echo "Please verify the plan and type yes"
+  read confirmation
+  if [[ $confirmation == 'yes' ]]
+  then
+    echo "Thank you. Applying the changes"
+    terraform apply plan.out
+  else
+    echo "Ok. Operation cancelled"
+    return 1
+  fi
+
+  if [[ $1 -eq 0 ]]
+  then
+    echo "$resource provisioned successfully"
+    return 0
+  else
+    echo "some error happened when applying the terraform scripts"
+    return 1
+  fi
+}
+
+function destroy() {
+  resource=$1
+  env=$2
+
+  check_env $resource $env
+
+  cd $resource
+  terraform destroy -var-file="../secrets/$env.tfvars"
+}
--- infrastructure_provisioning/deployer/ecr.sh
+++ infrastructure_provisioning/deployer/ecr.sh
@@ -5,36 +5,11 @@ set -e
 . ./deployer/common.sh
 
 function ecr_install() {
-  check_env 'ecr' $1
   env=$1
-
-  cd ecr
-  terraform plan -out plan.out -var-file="../secrets/$env.tfvars"
-  echo "Please verify the plan and type yes"
-  read confirmation
-  if [[ $confirmation == 'yes' ]]
-  then
-    echo "Thank you. Applying the changes"
-    terraform apply plan.out
-  else
-    echo "Ok. Operation cancelled"
-    return 1
-  fi
-
-  if [[ $1 -eq 0 ]]
-  then
-    echo "ecr provisioned successfully"
-    return 0
-  else
-    echo "some error happened when applying the terraform scripts"
-    return 1
-  fi
+  install 'ecr' $env
 }
 
 function ecr_destroy() {
-  check_env 'ecr' $1
   env=$1
-
-  cd ecr
-  terraform destroy -var-file="../secrets/$env.tfvars"
+  destroy 'ecr' $env
 }
