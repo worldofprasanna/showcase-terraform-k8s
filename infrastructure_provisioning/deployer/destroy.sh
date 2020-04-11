513d2f589f9e5587e107fe58c8d98acb3fc5d5c8
Prasanna V <prasannacse2005@gmail.com>
2020-04-11 13:48:48 +0530

    refactor the install scripts furthere

--- infrastructure_provisioning/deployer/destroy.sh
+++ infrastructure_provisioning/deployer/destroy.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-set -e
-
 if [[ $# -lt 2 ]]
 then
   echo "Usage: ./deployer/destroy.sh <resource | all> <env>"
@@ -10,33 +8,29 @@ fi
 resource=$1
 env=$2
 
-. ./deployer/ecr.sh
-. ./deployer/helm_charts.sh
+. ./deployer/common.sh
 
-case $resource in
-  ecr)
-    ecr_destroy $env
-  ;;
-  helm_charts)
-    helm_charts_destroy $env
-  ;;
-  vpc)
-    echo "Going to provision vpc for $env"
-  ;;
-  rds)
-    echo "Going to provision rds for $env"
-  ;;
-  eks)
-    echo "Going to provision eks for $env"
-  ;;
-  all)
-    echo "Going to provision all for $env"
-  ;;
-  *)
-    echo "Sorry you have given wrong input for resource. Check the usage again"
-    echo "Usage: ./deployer/destroy.sh <resource | all> <env>"
-    exit 1
-  ;;
-esac
+available_resources=('ecr' 'helm_charts' 'vpc' 'rds' 'eks' 'all')
+if [[ ! "${available_resources[@]}" =~ ${resource} ]]; then
+  echo "Sorry you have given wrong input for resource. Available resource [ ecr | helm_charts | vpc | rds | eks | all ]"
+  echo "Usage: ./deployer/destroy.sh <resource | all> <env>"
+  exit 1
+fi
+
+if [[ $resource == 'all' ]]
+then
+  for r in "${available_resources[@]}"
+  do
+    if [[ $r == 'all' ]]
+    then
+      continue
+    else
+      destroy $r $env
+    fi
+  done
+  echo "completed all the resources provisioning"
+else
+  destroy $resource $env
+fi
 
 cd ../
--- infrastructure_provisioning/deployer/install.sh
+++ infrastructure_provisioning/deployer/install.sh
@@ -8,33 +8,29 @@ fi
 resource=$1
 env=$2
 
-. ./deployer/ecr.sh
-. ./deployer/helm_charts.sh
+. ./deployer/common.sh
 
-case $resource in
-  ecr)
-    ecr_install $env
-  ;;
-  helm_charts)
-    helm_charts_install $env
-  ;;
-  vpc)
-    echo "Going to provision vpc for $env"
-  ;;
-  rds)
-    echo "Going to provision rds for $env"
-  ;;
-  eks)
-    echo "Going to provision eks for $env"
-  ;;
-  all)
-    echo "Going to provision all for $env"
-  ;;
-  *)
-    echo "Sorry you have given wrong input for resource. Available resource [ ecr | helm_charts | vpc | rds | eks | all ]"
-    echo "Usage: ./deployer/install.sh <resource | all> <env>"
-    exit 1
-  ;;
-esac
+available_resources=('ecr' 'helm_charts' 'vpc' 'rds' 'eks' 'all')
+if [[ ! "${available_resources[@]}" =~ ${resource} ]]; then
+  echo "Sorry you have given wrong input for resource. Available resource [ ecr | helm_charts | vpc | rds | eks | all ]"
+  echo "Usage: ./deployer/install.sh <resource | all> <env>"
+  exit 1
+fi
+
+if [[ $resource == 'all' ]]
+then
+  for r in "${available_resources[@]}"
+  do
+    if [[ $r == 'all' ]]
+    then
+      continue
+    else
+      install $r $env
+    fi
+  done
+  echo "completed all the resources provisioning"
+else
+  install $resource $env
+fi
 
 cd ../
